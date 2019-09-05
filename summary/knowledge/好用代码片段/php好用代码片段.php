2060fff25e5e1cc7ba5a38b6df09c7334e03d7c9
miracle <610010559@qq.com>
2019-09-06 01:02:02 +0800

    奶思

--- summary/knowledge/好用代码片段/php好用代码片段.php
+++ summary/knowledge/好用代码片段/php好用代码片段.php
@@ -149,3 +149,10 @@ function getSymbolByQuantity($bytes)
 
     return sprintf('%.2f ' . $symbols[$exp], ($bytes / pow(1024, floor($exp))));
 }
+
+function human_filesize($bytes, $decimals = 2)
+{
+    $sz = 'BKMGTP';
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
