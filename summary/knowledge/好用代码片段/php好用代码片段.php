60c3c868ae4036e2b860fe26825756ceefde5318
miracle <610010559@qq.com>
2019-09-06 00:45:02 +0800

    奶思

--- summary/knowledge/好用代码片段/php好用代码片段.php
+++ summary/knowledge/好用代码片段/php好用代码片段.php
@@ -139,3 +139,13 @@ class Excel
         ob_end_clean();
     }
 }
+
+################不用递归格式化字节为易读形式
+
+function getSymbolByQuantity($bytes)
+{
+    $symbols = array('B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
+    $exp = $bytes ? floor(log($bytes) / log(1024)) : 0;
+
+    return sprintf('%.2f ' . $symbols[$exp], ($bytes / pow(1024, floor($exp))));
+}
