913c54c68729aa2ab97a4f1e0140cdc7c3962b2d
zxd3 <zxd3@meitu.com>
2022-07-30 10:33:39 +0800

    good

--- summary/knowledge/case/js.js
+++ summary/knowledge/case/js.js
@@ -1,4 +1,4 @@
-//获取checkbox选中值的数组,都没选中返回空数组 [answer](https://stackoverflow.com/a/16171197/8714749)
+//获取checkbox选中值的数组,都没选中返回空数组 [answer](https://stackoverflow.com/a/16171197/8714749) [map常用方法](https://api.jquery.com/map/)
     $('.js-platform:checked').map(function () {
         return $(this).val();
     }).get().join()
