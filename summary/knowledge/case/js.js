8eb33b800febacbf308c8d00f6a0f024445928a0
zxd3 <zxd3@meitu.com>
2022-07-30 10:46:33 +0800

    good

--- summary/knowledge/case/js.js
+++ summary/knowledge/case/js.js
@@ -1,7 +1,7 @@
 //获取checkbox选中值的数组,都没选中返回空数组 [answer](https://stackoverflow.com/a/16171197/8714749) [map常用方法](https://api.jquery.com/map/)
     $('.js-platform:checked').map(function () {
         return $(this).val();
-    }).get().join()
+    }).get().join().toArray() //获取.toArray()也可
 
 //判断元素是否在数组中(类似inArray)
     arr = [1, 2, 3]
